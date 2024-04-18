398fb6f56e91f3f984f229b43c28aaf0474d1784
ustsl <w@imvo.site>
2024-04-18 14:18:03 +0300

    upd

--- main.py
+++ main.py
@@ -17,27 +17,6 @@ from starlette.middleware.cors import CORSMiddleware
 # create instance of the app
 app = FastAPI(title="ai_handler")
 
-
-# create the instance for the routes
-main_api_router = APIRouter(dependencies=[Depends(verify_token)])
-
-# set routes to the app instance
-main_api_router.include_router(prompt_router, prefix="/v1/prompts", tags=["prompts"])
-main_api_router.include_router(query_router, prefix="/v1/queries", tags=["queries"])
-main_api_router.include_router(user_router, prefix="/v1/users", tags=["users"])
-app.include_router(main_api_router)
-
-from fastapi import Depends, FastAPI
-import uvicorn
-from fastapi.routing import APIRouter
-from starlette.middleware.cors import CORSMiddleware
-
-from api.prompts.handlers import prompt_router
-from api.queries.handlers import query_router
-from api.users.handlers import user_router
-from api.utils import verify_token
-
-
 origins = [
     "http://78.180.37.41",
     "http://78.180.37.41:3000",
@@ -58,7 +37,11 @@ app.add_middleware(
 )
 
 
-app = FastAPI(title="ai_handler")
+# create the instance for the routes
+main_api_router = APIRouter(dependencies=[Depends(verify_token)])
 
-
-print("aihandler started")
+# set routes to the app instance
+main_api_router.include_router(prompt_router, prefix="/v1/prompts", tags=["prompts"])
+main_api_router.include_router(query_router, prefix="/v1/queries", tags=["queries"])
+main_api_router.include_router(user_router, prefix="/v1/users", tags=["users"])
+app.include_router(main_api_router)
