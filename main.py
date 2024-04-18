2cecf3385bf38e35ff283964652a07243c7dfa36
ustsl <w@imvo.site>
2024-04-18 13:33:35 +0300

    allow origins *

--- main.py
+++ main.py
@@ -17,14 +17,24 @@ from starlette.middleware.cors import CORSMiddleware
 # create instance of the app
 app = FastAPI(title="ai_handler")
 
+origins = [
+    "http://78.180.37.41",
+    "http://78.180.37.41:3000",
+    "http://host.docker.internal:3000",
+    "http://mc-angio.ru",
+    "https://mc-angio.ru",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # create the instance for the routes
 main_api_router = APIRouter(dependencies=[Depends(verify_token)])
