65f73b787532efbb67fbb9cf63a18746fe30b079
ustsl <w@imvo.site>
2024-05-21 22:49:12 +0300

    dalee

--- src/modules/gpt/modules/param.py
+++ src/modules/gpt/modules/param.py
@@ -5,12 +5,12 @@ model_price = {
     "gpt-4-turbo-2024-04-09": 0.08,
     "gpt-4o": 0.08,
     # SYMBOL & - is splitter for image models
-    "dall-e-3": 0.1,
+    "dall-e-3": 0.15,
 }
 
 
 dalee_style = {"natural": 0, "vivid": 0}
 
-dalee_size = {"1024x1024": 0, "1792x1024": 0.04, "1024x1792": 0.04}
+dalee_size = {"1024x1024": 0, "1792x1024": 0.05, "1024x1792": 0.05}
 
-dalee_quality = {"hd": 0.04}
+dalee_quality = {"hd": 0.05}
