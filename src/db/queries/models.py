3144f1ba668c1e248e0e10132258da37e0604cec
ustsl <w@imvo.site>
2024-05-23 16:11:28 +0300

    del time update from queries

--- src/db/queries/models.py
+++ src/db/queries/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Numeric, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
-
-from src.db.models import TimeModel
+from sqlalchemy.sql import func
+from src.db.models import Base
 
 import uuid
 
@@ -12,7 +12,7 @@ import uuid
 ##############################
 
 
-class QueryModel(TimeModel):
+class QueryModel(Base):
     __tablename__ = "queries"
     uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.uuid", ondelete="CASCADE"))
@@ -21,5 +21,6 @@ class QueryModel(TimeModel):
     )
     query = Column(String, nullable=False)
     result = Column(String, nullable=True)
+    time_create = Column(DateTime(timezone=True), default=func.now())
     user = relationship("UserModel", back_populates="queries", uselist=True)
     prompt = relationship("PromptModel", back_populates="queries", uselist=True)
